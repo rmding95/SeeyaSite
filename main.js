962ce8dcfa4771dc0f87502a1b9e0676ac5aca90
Richard Ding <richard.ding@alaskaair.com>
2017-01-04 02:33:30 -0800

    flights show up

--- main.js
+++ main.js
@@ -18,7 +18,17 @@ function getFlights() {
         url: url,
         contentType: "application/json",
         success: function(data) {
-            console.log(data)
+            console.log(data);
+            data.sort(function(a, b) {
+                if (a.length != b.length) {
+                    return a.length - b.length;
+                } else {
+                    var date1 = new Date(a[0].DepartureDate);
+                    var date2 = new Date(b[0].DepartureDate);
+                    return date1.getTime() - date2.getTime();
+                }
+            });
+            displayData(data);
             $("#results_loading").hide();
         },
         error: function(data) {
@@ -26,4 +36,24 @@ function getFlights() {
             $("#results_loading").hide();
         }
     });
+}
+
+function displayData(data) {
+    console.log(data);
+    for (var i = 0; i < data.length; i++) {
+        var flightScheduleDiv = $("<div></div>").attr("class", "col s12 card-panel hoverable").appendTo("#results");
+        for (var j = 0; j < data[i].length; j++) {
+            var flightSegmentDiv = $("<div></div>").attr("class", "col s12").appendTo(flightScheduleDiv);
+            var departureAirport = $("<div></div>").attr("class", "col s3 firstrow").text(data[i][j].DepartureAirport).appendTo(flightSegmentDiv);
+            var airplaneIcon = $("<i></i>").attr("class", "fa fa-plane firstrow").appendTo(flightSegmentDiv);
+            var arrivalAirport = $("<div></div>").attr("class", "col s3").text(data[i][j].ArrivalAirport).appendTo(flightSegmentDiv);
+
+            var departureDate = new Date(data[i][j].DepartureDate);
+            var arrivalDate = new Date(data[i][j].ArrivalDate);
+            var timeDiv = $("<div></div>").attr("class", "col s12").appendTo(flightScheduleDiv);
+            var departureTime = $("<div></div>").attr("class", "col s3 firstrow").text(departureDate.toLocaleTimeString()).appendTo(timeDiv);
+            var arrivalTime = $("<div></div>").attr("class", "col s3").text(arrivalDate.toLocaleTimeString()).appendTo(timeDiv);
+        }
+
+    }
 }
